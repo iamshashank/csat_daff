4ee8eb99c1cddfa0b04c1b5e1f11b85799fcff39
akamemelord <shashank.yadav@daffodilsw.com>
2018-09-17 12:04:25 +0530

    comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './component/App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from "./redux/store/store";
+//app initialized
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter><App /></BrowserRouter>
-    </Provider>,
-    document.getElementById('root'));
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById("root")
+);
+//react app initialized
